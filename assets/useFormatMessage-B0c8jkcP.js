f13cbea0f47427130854b49c2ae5bedab9087e72
Patryk Błaszczyk <patryk.blaszczyk@ekoenergetyka.com.pl>
2026-02-08 16:39:47 +0100

    Fix Storybook base path for GitHub Pages deployment

--- assets/useFormatMessage-B0c8jkcP.js
+++
@@ -1,1 +0,0 @@
-import{u as e,r as o}from"./iframe-BPOOcbSv.js";const n=(s,t)=>{const r=e();return o.useMemo(()=>r.formatMessage({id:s},t),[r,s,t])};export{n as u};
---
+++ assets/useFormatMessage-C2IZnRhR.js
@@ -0,0 +1,1 @@
+import{u as e,r as o}from"./iframe-9GXXH_tJ.js";const n=(s,t)=>{const r=e();return o.useMemo(()=>r.formatMessage({id:s},t),[r,s,t])};export{n as u};
